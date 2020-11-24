80e8681cc0cd6fc7c04b83e6659de834a98bb1af
henrysitu <imhenrysitu@gmail.com>
2020-11-24 13:10:34 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import version as version
+from setuptools import setup, find_packages
+
+setup(
+    name='MECH 45X PyTorch Image Classification',
+    version=version.__version__,
+    packages=find_packages(),
+    url='https://github.com/ChenyiZheng/45X_ML_Projects.git',
+    author='Henry Situ // MECH 45X',
+    description='Python repo for MECH 45X machine learning tutorials',
+    install_requires=[
+        'pandas >= 1.1.4',
+        'scikit-image',
+    ],
+)
