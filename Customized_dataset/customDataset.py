0b0fcbf02004c915bab2db8474d91b056337d2de
henrysitu <imhenrysitu@gmail.com>
2020-11-24 12:45:36 -0800

    Initial PrepareDataset class push

--- Customized_dataset/customDataset.py
+++ Customized_dataset/customDataset.py
@@ -1,1 +1,17 @@
-# Init Commit
+import os
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+from skimage import io
+
+class PrepareDataset(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.annotations = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
