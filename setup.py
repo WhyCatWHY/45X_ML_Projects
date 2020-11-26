087c2cf395c55836c441744feafe226d76940b51
henrysitu <imhenrysitu@gmail.com>
2020-11-26 15:10:52 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ setup(
     version=version.__version__,
     packages=find_packages(),
     url='https://github.com/ChenyiZheng/45X_ML_Projects.git',
-    author='Henry Situ // MECH 45X',
+    author='Chenyi Zheng // Henry Situ // Team 10 // MECH 45X',
     description='Python repo for MECH 45X machine learning tutorials',
     install_requires=[
         'pandas >= 1.1.4',
         'scikit-image',
+        'numpy',
     ],
 )
